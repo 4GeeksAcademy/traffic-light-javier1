78a26ec335bb1da63fbdc2d94c4b5260315246d5
Javier Gracia <107503117+Javiergracia47@users.noreply.github.com>
2024-11-06 21:43:57 +0000

    proyecto finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+const [color, setColor] = userState(null);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="traffic-signal"> 
+		<div className={`light red ${color == "0" ? "glow":""}`} onClick={()=>setColor(0)}>
+
+			</div>	
+		<div className={`light yellow ${color == "1" ? "glow":""}`} onClick={()=>setColor(1)}>
+			</div>	
+		<div className={`light green ${color == "2" ? "glow":""}`} onClick={()=>setColor(2)}>
+			</div> 
+	</div>
 	);
 };
 
